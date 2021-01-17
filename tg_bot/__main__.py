c64d4e3791fb7b932687e0d845ea423a53eff8df
savageuser <77573885+savageuser@users.noreply.github.com>
2021-01-17 18:00:27 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -49,7 +49,7 @@ Hello! my name *{}*.
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-TECHNO_IMG = "https://telegra.ph/file/84b2017bc2f3c90f2e61c.jpg"
+TECHNO_IMG = "https://lh3.googleusercontent.com/-E2mMPO3z7dE/YAQthK8ojjI/AAAAAAAABus/cZZdAppL7lgiyjuKaSpc9Vwdh8J7L59wgCLcBGAsYHQ/s1600/1610886519617412-0.png"
 IMPORTED = {}
 MIGRATEABLE = []
 HELPABLE = {}
@@ -142,7 +142,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🤝HELP🤝",
                                                                        callback_data="help_back".format(bot.username)),
                                                                                    InlineKeyboardButton(text="🧑‍💻My Creator🧑‍💻",
-                                                                       url="t.me/teamishere")],
+                                                                       url="t.me/teamie")],
                                                                                    [InlineKeyboardButton(text="ADD GRAND OFFICIAL TO YOUR GROUP",
                                                                        url="t.me/{}?startgroup=true".format(bot.username)),
                                                                                    InlineKeyboardButton(text="Source Code",
